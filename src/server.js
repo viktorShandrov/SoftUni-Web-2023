c06d4c33a75f6586718f2685bb0bf8fd862d8ed1
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-05-24 11:29:11 +0300

    Setting home router

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const expressConfig = require("../config/expressConfig")
-
+const handlebarsConfig = require("../config/handlebarsConfig")
+const homeController = require("./controllers/homeController")
 const server = express();
 
 expressConfig(server);
 handlebarsConfig(server);
 
+server.use(homeController);
 
-server.listen(5000,()=>"Server is listening on port 5000...")
+
+server.listen(5000,()=>console.log("Server is listening on port 5000..."))
