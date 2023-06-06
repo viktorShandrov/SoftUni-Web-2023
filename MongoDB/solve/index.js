b907a814648664ea6f6a12940f4289b142e851ae
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-06-06 17:06:06 +0300

    Move

---
+++ MongoDB/solve/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const User = require("./user");
+const { log } = require("console");
+async function connectDB(){
+    mongoose.connect("mongodb://127.0.0.1:27017/Test");
+    await User.create({name:"Spas",age:10})
+    log(await User.find())
+}
+connectDB()
